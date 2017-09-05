3f67598c2afa3e915452b0a2c1bcca0408f34ca8
shira <shira@beengo.me>
2017-09-06 01:18:21 +0300

    Added basics for landing page

--- HavaClient/src/components/App.js
+++ HavaClient/src/components/App.js
@@ -7,8 +7,21 @@ export default class App extends Component {
     render() {
         return (
             <div>
-                <div className="navigation"></div>
-                <div className="landing"></div>
+                <div className="header-container">
+                    <img className="logo" src="/static/images/logo.png" />
+                    <div className="nav">
+
+                    </div>
+                </div>
+                <div className="landing">
+                    <div className="background-container">
+                        <img className="background" src="/static/images/market.jpg" />
+                        {/*<video autoplay className="background" loop>*/}
+                            {/*<source src="/static/images/market.mp4" type="video/mp4" />*/}
+                            {/**/}
+                        {/*</video>*/}
+                    </div>
+                </div>
 
             </div>
         );
